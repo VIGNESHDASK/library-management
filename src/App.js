f6d7fa5f922abf53c43cc726623d6c3af27d8613
VIGNESHDASK <vigneshdas4444@gmail.com>
2025-01-01 00:01:43 +0530

    delted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import MFundData from './components/MFundData'
+import ArrayComponent from './components/ArrayComponent'
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <MFundData />
+      <ArrayComponent />
     </div>
   );
 }
