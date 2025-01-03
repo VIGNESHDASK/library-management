e524b4f6795cb7c91079640b98993b6d13d5ba80
VIGNESHDASK <vigneshdas4444@gmail.com>
2025-01-03 22:25:06 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ArrayComponent from './components/ArrayComponent'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 
 
